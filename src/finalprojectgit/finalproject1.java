9bcd9e656f24b22f6c4d7ef561ed87f103cd6804
athirajanardanan <athi.j89@gmail.com>
2022-09-28 20:48:05 -0400

    second commit

---
+++ src/finalprojectgit/finalproject1.java
@@ -0,0 +1,11 @@
+package finalprojectgit;
+
+public class finalproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adding a new class");
+
+	}
+
+}
