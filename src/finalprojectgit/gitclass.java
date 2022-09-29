1518d2e26629f1492bf7e48093d437692ae05ea9
athirajanardanan <athi.j89@gmail.com>
2022-09-28 20:11:03 -0400

    first commit

---
+++ src/finalprojectgit/gitclass.java
@@ -0,0 +1,11 @@
+package finalprojectgit;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a code");
+
+	}
+
+}
